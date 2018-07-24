10b475e481b9a26e5db5360343fdb8e376d3aea4
diana <dyc3@njit.edu>
2018-07-24 18:36:51 -0400

    Added question migrate, model, and updated the user seeder and added question test

--- app/User.php
+++ app/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
     {
         return $this->hasOne('App\Profile');
     }
+
+    public function questions()
+    {
+        return $this->hasMany('App\Question');
+    }
 }
